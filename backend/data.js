d628de7c05e72d19d2233164d07525461355d676
sliCe <quocthaitg11@gmail.com>
2022-11-20 19:33:18 +0700

    Video-21.-Seed-Sample-Users

--- backend/data.js
+++ backend/data.js
@@ -1,4 +1,20 @@
+import bcrypt from 'bcryptjs';
+
 const data = {
+  users: [
+    {
+      name: 'Thai',
+      email: 'admin@example.com',
+      password: bcrypt.hashSync('123456'),
+      isAdmin: true,
+    },
+    {
+      name: 'Thien',
+      email: 'user@example.com',
+      password: bcrypt.hashSync('123456'),
+      isAdmin: false,
+    },
+  ],
   products: [
     {
       // _id: '1',
