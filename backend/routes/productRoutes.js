380a575eaf063a55b83a90a28fbe765fc4ea2f51
sliCe <quocthaitg11@gmail.com>
2022-11-28 16:27:01 +0700

    Video-34-Add-Sidebar-and-Search-Box

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,12 +1,20 @@
 import express from 'express';
+import expressAsyncHandler from 'express-async-handler';
 import Product from '../models/productModel.js';
 
 const productRouter = express.Router();
-
 productRouter.get('/', async (req, res) => {
   const products = await Product.find();
   res.send(products);
 });
+
+productRouter.get(
+  '/categories',
+  expressAsyncHandler(async (req, res) => {
+    const categories = await Product.find().distinct('category');
+    res.send(categories);
+  })
+);
 
 productRouter.get('/slug/:slug', async (req, res) => {
   const product = await Product.findOne({ slug: req.params.slug });
@@ -24,5 +32,4 @@ productRouter.get('/:id', async (req, res) => {
     res.status(404).send({ message: 'Product Not Found' });
   }
 });
-
 export default productRouter;
